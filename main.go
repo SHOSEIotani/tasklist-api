0b1b7c6be14babf080bf969f41c4dea5f1c12681
大谷翔生 <otanishosei@ooyashouseinoMacBook-Pro.local>
2022-08-25 16:58:03 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+type Task struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Done bool   `json:"done"`
+}
+
+var tasks = make([]Task, 0)
+
+func main() {
+	r := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	r.Use(cors.New(config))
+
+	r.GET("/tasks", func(c *gin.Context) {
+		c.JSON(http.StatusOK, tasks)
+	})
+
+	r.POST("/tasks", func(c *gin.Context) {
+		var task Task
+		c.BindJSON(&task)
+
+		task.ID = strconv.Itoa(rand.Int())
+
+		tasks = append(tasks, task)
+
+		c.Status(http.StatusCreated)
+	})
+
+	r.PUT("/tasks/:id", func(c *gin.Context) {
+		var newTask Task
+		c.BindJSON(&newTask)
+
+		for i, task := range tasks {
+			if task.ID == c.Param("id") {
+				newTask.ID = task.ID
+				tasks[i] = newTask
+				break
+			}
+		}
+
+		c.Status(http.StatusOK)
+	})
+
+	r.DELETE("/tasks/:id", func(c *gin.Context) {
+		for i, task := range tasks {
+			if task.ID == c.Param("id") {
+				tasks = append(tasks[:i], tasks[i+1:]...)
+				break
+			}
+		}
+
+		c.Status(http.StatusOK)
+	})
+
+	r.Run()
+}
